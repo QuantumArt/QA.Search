2523d24004e0a9c2c493ef6da6a34a8718125346
oJieg <oleg151-13@mail.ru>
2024-01-07 19:32:45 +0300

    Fix parse date

--- QA.Search.Admin/ClientApp/src/utils/time.ts
+++ QA.Search.Admin/ClientApp/src/utils/time.ts
@@ -1,27 +1,13 @@
-const offset: number = new Date().getTimezoneOffset();
+import dateAndTime from "date-and-time";
+import twoDigitYear from "date-and-time/plugin/two-digit-year";
 
 export default function dateConvertToLocal(dateString: string | null | undefined): string {
   if (!dateString) {
     return "";
-  } 
+  }
 
-  let date: Date = parseStringToDate(dateString);
-  date.setMinutes(date.getMinutes() - offset);
+  dateAndTime.plugin(twoDigitYear);
+  const date = dateAndTime.parse(dateString, "DD.MM.YY HH:mm:ss", true);
 
   return date.toLocaleString();
-}
-
-function parseStringToDate(dateString: string): Date {
-  let dateAndTime: string[] = dateString.split(" ");
-  let date: string[] = dateAndTime[0].split(".");
-  let time = dateAndTime[1].split(":");
-
-  return new Date(
-    parseInt("20" + date[2]),
-    parseInt(date[1], 10) - 1,
-    parseInt(date[0]),
-    parseInt(time[0]),
-    parseInt(time[1]),
-    parseInt(time[2])
-  );
 }
